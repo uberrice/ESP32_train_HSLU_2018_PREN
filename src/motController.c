bb765dbfd36d4d41446cf4da480c9c1defd3f194
uberrice <overrice@mail.ru>
2019-05-24 09:49:52 +0200

    switched typo

--- src/motController.c
+++ src/motController.c
@@ -18,7 +18,7 @@ static double oldtime = 0;
 int32_t targetRPM = 0;
 int32_t intrig = 0;
 uint8_t controlEnable = 1;
-motDir_t motdir = REVERSE;
+motDir_t motdir = BACKWARD;
 
 
 void setMotDir(motDir_t t){
