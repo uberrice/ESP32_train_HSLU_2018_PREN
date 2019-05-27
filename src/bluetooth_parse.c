76bb72cec271c48dcde72ed766a8d14c39daa4c5
uberrice <overrice@mail.ru>
2019-05-27 16:00:03 +0200

    added experimental beep parser - TO BE TESTED

--- src/bluetooth_parse.c
+++ src/bluetooth_parse.c
@@ -6,8 +6,15 @@
 #include "freertos/task.h"
 #include "taskhandles.h"
 
+char* p;
 void btParse(char* buf){
     if(strcmp("Hello",buf) == 0){
     xTaskNotify(beepHandle,3,eSetValueWithOverwrite);
+    }
+
+    if(strncmp("beep ", buf, sizeof("beep ")-1) == 0){
+        p = buf + sizeof("beep ") - 1;
+        uint32_t b = (uint32_t)abs(atoi(p));
+        xTaskNotify(beepHandle,b,eSetValueWithOverwrite);
     }
 }
