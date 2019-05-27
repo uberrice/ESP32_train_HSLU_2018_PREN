9057a4b523b0afea6f36562ccbd169a82c9b9b4a
uberrice <overrice@mail.ru>
2019-05-27 16:11:30 +0200

    added maximum number of beeps

--- src/bluetooth_parse.c
+++ src/bluetooth_parse.c
@@ -6,15 +6,18 @@
 #include "freertos/task.h"
 #include "taskhandles.h"
 
-char* p;
+
 void btParse(char* buf){
+    char* ptr;
     if(strcmp("Hello",buf) == 0){
     xTaskNotify(beepHandle,3,eSetValueWithOverwrite);
     }
 
     if(strncmp("beep ", buf, sizeof("beep ")-1) == 0){
-        p = buf + sizeof("beep ") - 1;
-        uint32_t b = (uint32_t)abs(atoi(p));
-        xTaskNotify(beepHandle,b,eSetValueWithOverwrite);
+        ptr = buf + sizeof("beep ") - 1;
+        uint32_t b = (uint32_t)abs(atoi(ptr));
+        if(b < 10){
+            xTaskNotify(beepHandle,b,eSetValueWithOverwrite);
+        }
     }
 }
